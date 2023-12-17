fcbe06bc5f3f1d302083375792cfb3c5a6896ac9
GADGETNiK <87785095+GADGETNiK@users.noreply.github.com>
2023-12-18 01:26:12 +0300

    Update auto_createjson_9.x.sh

--- auto_createjson_9.x.sh
+++ auto_createjson_9.x.sh
@@ -49,7 +49,7 @@ echo '{
         "oem": "'$oem'",
         "device": "'$devicename'",
         "filename": "'$zip_only'",
-        "download": "https://github.com/GADGETNiK/crdroid_ota_oss_nonksu/releases/download/'$nozip'/'$zip'",
+        "download": "https://github.com/GADGETNiK/crdroid_ota_oss_ksu/releases/download/'$nozip'/'$zip'",
         "timestamp": '$timestamp',
         "md5": "'$md5'",
         "sha256": "'$sha256'",
